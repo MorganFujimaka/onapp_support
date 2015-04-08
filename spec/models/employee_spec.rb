4d106fe51b78e3906f5f73a6c11e213033076729
Oleg Kariuk <olegkariuk@gmail.com>
2015-04-08 18:11:29 +0300

    Adds specs for controllers

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
+  context 'method delegation' do
+    it 'responds to #username' do
+      employee = create :employee
+
+      expect(employee).to respond_to(:username)
+    end
+  end
 end
